29a40c0bd9984ecfc57b8b4c9454897ccc9f614d
Hector Beo <hectordbeo@gmail.com>
2021-05-10 11:19:01 -0300

    finalizacao do modulo HMTL avancado

--- HTML Avancado/script2.js
+++ HTML Avancado/script2.js
@@ -1,8 +1,7 @@
 let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
-let ctx2 = tela.getContext("2d");
-
+/*
 ctx.beginPath();
 ctx.moveTo(50, 50);
 ctx.lineTo(100,100)
@@ -18,6 +17,45 @@ ctx.lineWidth = 5;
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.stroke();
+*/
+
+
+
+let circle = {
+    x : 250,
+    y : 250,
+    raio : 100,
+    inicio : 0,
+    fim : 0
+    
+}
+
+function drawCircle(c){
+    ctx.beginPath()
+    ctx.strokeStyle = "red"
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+    ctx.fillStyle = "blue"
+    
+    ctx.stroke();
+    ctx.fill();
+}
+
+drawCircle(circle);
+
+setInterval(function(){
+
+    if(circle.fim < 2 * Math.PI)
+    {
+        circle.fim += 0.3;
+        circle.x += 5;
+    }
+
+    drawCircle(circle);
+
+
+}, 50);
+
+
 
 
 
